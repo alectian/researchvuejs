c4a67391230d6955139123cb428e3c55a38a8e69
alectian <alectian@icloud.com>
2019-07-31 18:25:59 +0800

    v-model数据双向绑定

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,17 @@
 <template>
   <div id="app">
-    
+    <input v-model="name" placeholder='你的名字'>
+    <h1>你好{{ name }}</h1> 
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      name: ""
+    }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
