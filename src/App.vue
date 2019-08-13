fa187e80e4cc9eaf969202f7d7826417cf0db5f8
alectian <alectian@icloud.com>
2019-08-13 16:55:06 +0800

    计算属性getter

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-   {{ reverseText }}
+    总价：{{ prices }}
   </div>
 </template>
 
@@ -8,23 +8,70 @@
   export default{
     data(){
       return{
-        text:'123,456'
+        package1:[
+          {
+            name:'iPhone',
+            price:7199,
+            count:2
+          },{
+            name:'iPad',
+            price:2888,
+            count:1
+          },{
+            name:'Mac',
+            price:19199,
+            count:10
+          },{
+            name:'Pod',
+            price:1250,
+            count:20
+          }
+        ],
+        package2:[
+          {
+            name:'Google',
+            price:1000,
+            count:51
+          },{
+            name:'Glass',
+            price:12888,
+            count:13
+          },{
+            name:'Phone',
+            price:5000,
+            count:19
+          },{
+            name:'Robot',
+            price:3000000,
+            count:44
+          }
+        ]
       }
     },
-    // 计算属性
-    /*
-    所有的计算属性都以函数的形式写在computed内，
-    最终返回计算结果
-    在一个计算属性里可以完成各种复杂的逻辑，
-    包括运算、函数调用等，
-    只要是最终返回一个结果就可以，
-    
-    除了以上简单的用法，计算属性还可以依赖多个vue实例的数据，
-    只要其中任一数据变化，计算属性就会重新执行，视图也会更新
-    */
     computed:{
-      reverseText: function(){
-        return this.text.split(',').reverse().join(',')
+      /*
+        当package1或package2中的商品有任何变化，
+        比如购买数量变化或增删商品时，
+        计算属性price就会自动更新，
+        视图中的总价也会自动变化，
+        每一个计算属性都包含一个getter和一个setter，
+        默认用法只利用了getter来读取
+        
+        
+        
+        在需要的时候，也可以提供一个setter函数，
+        当手动修改计算属性的值就像修改一个普通数据那样时，
+        就会触发setter函数，执行一些自定义操作
+      */
+      prices: function(){
+        var price = 0
+        for (var i = 0; i<this.package1.length; i++) {
+          price += this.package1[i].price*this.package1[i].count
+        }
+        for (var i = 0; i<this.package2.length; i++) {
+          price += this.package2[i].price*this.package2[i].count
+        }
+        return price
       }
     }
   }
