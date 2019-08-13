3b938e4c55404d163816cb3ffb9e365976cad982
alectian <alectian@icloud.com>
2019-08-13 13:43:03 +0800

    过滤器

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,53 @@
 <template>
   <div id="app">
-    <!-- v-pre跳过这个元素和她的子元素的编译过程 -->
-    <span v-pre>
-      {{这个内容不被编译}}
-      <div>
-        <!-- 这个元素的内容会直接输出 -->
-        {{ isOK ? '确定' : '取消' }}
-      </div>
-    </span>
-    <div>
-      <!-- 这个元素会进行编译 -->
-      <!-- 
-        vue只支持单个表达式，不支持流程控制，
-        另外，在表达式中不能使用用户自定义的全局变量，
-        只能使用vue白名单内的全局变量，
-        例如，Math和Date
-       〈!一这是语旬，不是表达式 一 〉
-        {{ var book = ’Vue.js实战’ }} 
-       〈!一不能使用流控制，要使用三元运算 一〉 
-        {{ if (ok) return msg ))
-       -->
-      {{ isOK ? '确定' : '取消' }}
-    </div>
+    <!--
+      vue支持在{{}}插值的尾部添加一个管道符号 | 对数据进行过滤，
+      经常用于格式化文本
+      比如，字母全大写，货币千位使用逗号分隔符等
+      过滤规则是自定义的，通过给vue实例添加选项filters来设置，
+     -->
+    {{ date | formatDate }}
   </div>
 </template>
 
 <script>
-  export default {
-    data:{
-      isOK: false
+  var padDate = function(value) {
+    return value < 10 ? '0' + value : value
+  }
+  export default{
+    data() {
+      return {
+        date: new Date()
+      }
+    },
+    filters:{
+      formatDate: (value)=>{
+        var date = new Date(value);
+        var year= date .getFullYear() ;
+        var month= padDate(date.getMonth() + 1) ;
+        var day= padDate(date .getDate());
+        var hours= padDate(date .getHours());
+        var minutes = padDate (date .getMinutes ());
+        var seconds = padDate (date .getSeconds ());
+        var str = year + '-' + month + '-' + day + ' ' + hours + ':' +minutes + ':' + seconds
+        return str//将整理好的数据返回出去
+      },
+      /*
+      过滤器应当用于处理简单的文本转换，
+      如果要实现更为复杂的数据变换，
+      应该使用计算属性
+      */
+    },
+    mounted() {
+      var _this = this
+      this.timer = setInterval(()=>{
+        _this.date = new Date()
+      }, 1000)
+    },
+    beforeDestroy(){
+      if (this.timer) {
+        clearInterval(this.timer)//在vue实例销毁前，清除定时器
+      }
     }
   }
 </script>
