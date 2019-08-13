aeab77ea6f04f43aa982a4ef274c0c2e51e9f863
alectian <alectian@icloud.com>
2019-08-13 15:46:55 +0800

    指令与事件

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,62 @@
 <template>
   <div id="app">
     <!--
-      vue支持在{{}}插值的尾部添加一个管道符号 | 对数据进行过滤，
-      经常用于格式化文本
-      比如，字母全大写，货币千位使用逗号分隔符等
-      过滤规则是自定义的，通过给vue实例添加选项filters来设置，
+      指令是vue模板中最常用的一项功能，
+      它带有v-前缀，
+      指令的主要职责就是当其表达式的值改变时，
+      相应地将某些行为应用到DOM上
      -->
-    {{ date | formatDate }}
+     <!--
+      当show的值为true时，
+      div元素会被自动插入，
+      为false时会被移除，
+
+      数据驱动DOM是vue的核心理念，所以不到万不得已时不要主动操作DOM
+      比如循环渲染、显示与隐藏等。
+
+      -->
+    <div v-if="show">闪烁 v-if指令</div>
+
+    <!-- v-bind的基本用途是动态更新HTML元素上的属性，比如id、class等 -->
+    <div>
+      v-bind指令
+      <a v-bind:href="url">链接</a>
+      <img v-bind:src="imgUrl"/>
+      <!-- 渲染后的结果为
+       <a href="https://www.github.com"〉链接</ a>
+       <img src="http://xxx.xxx .xx/img.png">
+       -->
+    </div>
+
+    <!-- v-on用来绑定事件监听器，这样就能做一些交互了 -->
+    <div>
+      v-on指令
+      <div v-if="close">显示一段文本</div>
+      <button v-on:click="handleClose">点击隐藏</button>
+    </div>
+
+
   </div>
 </template>
 
 <script>
-  var padDate = function(value) {
-    return value < 10 ? '0' + value : value
-  }
   export default{
-    data() {
-      return {
-        date: new Date()
+    data(){
+      return{
+        show:false,
+        close:true,
+        url:'https://www.baidu.com',
+        imgUrl:'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2174909441,2495215020&fm=26&gp=0.jpg'
       }
     },
-    filters:{
-      formatDate: (value)=>{
-        var date = new Date(value);
-        var year= date .getFullYear() ;
-        var month= padDate(date.getMonth() + 1) ;
-        var day= padDate(date .getDate());
-        var hours= padDate(date .getHours());
-        var minutes = padDate (date .getMinutes ());
-        var seconds = padDate (date .getSeconds ());
-        var str = year + '-' + month + '-' + day + ' ' + hours + ':' +minutes + ':' + seconds
-        return str//将整理好的数据返回出去
-      },
-      /*
-      过滤器应当用于处理简单的文本转换，
-      如果要实现更为复杂的数据变换，
-      应该使用计算属性
-      */
-    },
     mounted() {
-      var _this = this
-      this.timer = setInterval(()=>{
-        _this.date = new Date()
-      }, 1000)
+      setInterval(()=>{
+        this.show = !this.show
+      },1000)
     },
-    beforeDestroy(){
-      if (this.timer) {
-        clearInterval(this.timer)//在vue实例销毁前，清除定时器
+    methods:{
+      handleClose(){
+        this.close = !this.close
       }
     }
   }
