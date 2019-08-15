e947f83f2884deaaa5a9af91ee96387bbc750dcf
alectian <alectian@icloud.com>
2019-08-15 13:33:32 +0800

    了解v-bind指令

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <template>
   <div id="app">
-    姓名：{{ fullName }}
-    <button @click="changename">改名</button>
+    <!--
+      v-bind主要用法是动态更新HTML元素上的属性
+    -->
+    <!-- 
+      链接的href属性和图片的src属性都被动态设置了
+      当数据变化时，就会重新渲染。
+     -->
+    <a v-bind:href="link">链接</a>
+    <img v-bind:src="src"/>
   </div>
 </template>
 
@@ -9,37 +16,10 @@
   export default{
     data(){
       return{
-        firstName:'alec',
-        lastName:'tian'
-      }
-    },
-    methods:{
-      changename(){
-        this.fullName = 'lina-wang'
-      }
-    },
-    computed:{
-      /*
-        计算属性缓存：
-        计算属性是依赖缓存的，
-        一个计算属性所依赖的数据发生变化时，
-        它才会重新取值
-        
-        使用计算属性还是methods取决于你是否需要缓存，
-        当遍历大数组和做大量计算时，
-        应当使用计算属性，
-        除非你不希望得到缓存
-      */
-      fullName: {
-        get: function(){
-          return this.firstName + '-' + this.lastName
-        },
-        set:function(newValue){
-          var names = newValue.split('-')
-          this.firstName = names[0]
-          this.lastName = names[1]
-        }
+        link:'https://www.baidu.com',
+        src:'assets/logo.png'
       }
     }
+
   }
 </script>
